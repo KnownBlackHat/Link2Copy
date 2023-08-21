24aad10b9ea35f709aa53c48f838b7c8a9311b75
knownblackhat <knownblackhat@offsec>
2023-08-21 08:03:34 -0400

    chore: initial commit

---
+++ src/popup.js
@@ -0,0 +1,3 @@
+import Popup from './routes/Popup.svelte'
+
+new Popup({ target: document.body });
---
+++ src/routes/Popup.svelte
@@ -0,0 +1,108 @@
+<script>
+import browser from "webextension-polyfill";
+let links = [];
+
+browser.runtime.onMessage.addListener((message) => {
+    if (message.action === "update") {
+        links = message.links;
+    }
+});
+
+ async function getStartStatus() {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    const response = await browser.tabs.sendMessage(tabs[0].id, { action: "status" });
+    return response.started;
+  }
+
+function downloadLinksToFile(links) {
+    const filename = "media_links.txt";
+    const content = links.join('\n');
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+
+    setTimeout(() => {
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }, 0);
+}
+
+
+</script>
+<div class="p-4 bg-black text-white">
+<div class="text-2xl text-center font-bold mb-4">
+    Link2Copy
+    <div class="text-xs">
+        By Known Black Hat
+    </div>
+</div>
+
+
+{#await getStartStatus()}
+    Getting status
+{:then stat}
+
+{#if stat}
+    <div class="text-center text-blue-600 m-2">
+       Links Found: {links.length}
+    </div>
+    <button on:click={(e) => {
+        navigator.clipboard.writeText(links.join('\n'))
+        e.target.innerText = "Copied"
+        setTimeout(() => {
+            e.target.innerText = "Copy"
+        }, 1000);
+        }}
+    class="bg-gray-500 p-2 hover:bg-green-600 text-white rounded text-sm">
+    Copy
+    </button>
+        <button on:click={(e) => {
+            downloadLinksToFile(links)
+            e.target.innerText = "Downloaded"
+            setTimeout(() => {
+                e.target.innerText = "Download"
+            }, 1000);
+        }}
+    class="bg-gray-500 p-2 hover:bg-yellow-600 text-white rounded text-sm">
+    Download
+    </button>
+
+    <button on:click={(e) => {
+        e.target.innerText = "Stopped"
+        e.target.disabled = true
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            browser.tabs.sendMessage(tabs[0].id, { action: "stop" })
+            });
+        window.close()
+        }}
+    class="bg-gray-500 p-2 hover:bg-red-600 text-white rounded text-sm">
+        Stop
+    </button>
+
+{:else}
+
+    <div class="text-center">
+    <button on:click={(e) => {
+        e.target.innerText = "Started"
+        e.target.disabled = true
+    browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            browser.tabs.sendMessage(tabs[0].id, { action: "start" })
+            });
+        window.close()
+        }}
+    class="bg-gray-500 text-center p-2 hover:bg-blue-600 text-white rounded text-sm">
+        Start
+    </button>
+    </div>
+{/if}
+{:catch}
+    <div class="text-center text-red-600">
+        Only works on <a href="https://discord.com/channels/@me">Discord</a>
+    </div>
+{/await}
+</div>
